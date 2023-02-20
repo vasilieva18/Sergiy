96a393d6d9771c9da42c0632f690b2cc7513d750
vasilieva18 <masha.vasilieva1990@gmail.com>
2023-02-20 13:06:02 +0200

    commits
    new changes

---
+++ ForSerioga/src/study/Main.java
@@ -0,0 +1,9 @@
+package study;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Mashunia SeriaginaMashunia = new Mashunia("Mashunia Vasilieva", 32);
+        System.out.println(SeriaginaMashunia.toString());
+    }
+}
---
+++ ForSerioga/src/study/Mashunia.java
@@ -0,0 +1,15 @@
+package study;
+
+public class Mashunia implements Adekvatnist{
+    String name;
+    int age;
+    Mashunia(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public String toString()
+    {
+        return "Машуня умная и адекватная писюня:)";
+    }
+}
